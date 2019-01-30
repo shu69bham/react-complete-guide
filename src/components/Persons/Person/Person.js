52a743712cd29ffb710ab84f86aad7edc6a1b4bc
shu69bham <shubhamsrivastava746@gmail.com>
2019-01-31 00:08:25 +0530

    added node files

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const person = (props) => {    
-    return(        
-        <div>
-            <p onClick={props.click}>I'm {props.name}! {props.children}</p>
-            <input type="text" value = {props.name} onChange={props.changed} />
-        </div>        
-    );
-}
+const person = props => {
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm {props.name}! {props.children}
+      </p>
+      <input type="text" value={props.name} onChange={props.changed} />
+    </div>
+  );
+};
 
-export default person
+export default person;
